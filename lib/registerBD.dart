13630f335bd804ed88a5764ebbb6e7214b965969
JuanPabloSaavedraRamirez <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2024-10-15 09:38:11 -0600

    hide unos cambios en mi

--- lib/registerBD.dart
+++ lib/registerBD.dart
@@ -12,29 +12,17 @@ class RegisterBD extends StatefulWidget {
 
 class _RegisterStateBD extends State<RegisterBD> {
   final TextEditingController emailController = TextEditingController();
-  final TextEditingController confirmEmailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController ageController = TextEditingController();
   final TextEditingController numberController = TextEditingController();
 
   bool _validateFields() {
     return emailController.text.isNotEmpty &&
-        confirmEmailController.text.isNotEmpty &&
         passwordController.text.isNotEmpty &&
-        confirmPasswordController.text.isNotEmpty &&
         usernameController.text.isNotEmpty &&
         ageController.text.isNotEmpty &&
         numberController.text.isNotEmpty;
-  }
-
-  bool _validateEmails() {
-    return emailController.text == confirmEmailController.text;
-  }
-
-  bool _validatePasswords() {
-    return passwordController.text == confirmPasswordController.text;
   }
 
   void _showError(String message) {
@@ -47,13 +35,6 @@ class _RegisterStateBD extends State<RegisterBD> {
     );
   }
 
-  Future<void> _saveData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('email', emailController.text);
-    await prefs.setString('username', usernameController.text);
-    await prefs.setString('age', ageController.text);
-    await prefs.setString('number', numberController.text);
-  }
 
 
   void _onComplete() async {
@@ -61,16 +42,7 @@ class _RegisterStateBD extends State<RegisterBD> {
       ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text("Debes llenar todos los campos"),
             backgroundColor: Colors.red,));
-    } else if (!_validateEmails()) {
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Los correos no coinciden. Por favor, verifica tus datos."),
-            backgroundColor: Colors.red,));
-    } else if (!_validatePasswords()) {
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Las contraseñas no coinciden. Por favor, verifica tus datos."),
-            backgroundColor: Colors.red,));
     } else {
-      await _saveData();
       Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) {
         return Perfil(
           username: usernameController.text,
@@ -114,35 +86,10 @@ class _RegisterStateBD extends State<RegisterBD> {
                   margin: EdgeInsets.all(10),
                   color: Colors.white,
                   child: TextField(
-                    controller: confirmEmailController,
-                    decoration: InputDecoration(
-                      hintText: "Confirmar correo",
-                      hintStyle: TextStyle(color: Colors.grey),
-                      prefixIcon: Icon(Icons.email, size: 20),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(10),
-                  color: Colors.white,
-                  child: TextField(
                     controller: passwordController,
                     obscureText: true,
                     decoration: InputDecoration(
                       hintText: "Contraseña",
-                      hintStyle: TextStyle(color: Colors.grey),
-                      prefixIcon: Icon(Icons.lock, size: 20),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(10),
-                  color: Colors.white,
-                  child: TextField(
-                    controller: confirmPasswordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Confirmar contraseña",
                       hintStyle: TextStyle(color: Colors.grey),
                       prefixIcon: Icon(Icons.lock, size: 20),
                     ),
@@ -165,7 +112,7 @@ class _RegisterStateBD extends State<RegisterBD> {
                   color: Colors.white,
                   child: TextField(
                     controller: ageController,
-                    keyboardType: TextInputType.number,
+                    //keyboardType: TextInputType.number,
                     decoration: InputDecoration(
                       hintText: "Fecha de nacimiento",
                       hintStyle: TextStyle(color: Colors.grey),
@@ -209,7 +156,7 @@ class _RegisterStateBD extends State<RegisterBD> {
                     ),
                   ),
                 ),
-                Container(
+                /*Container(
                   margin: EdgeInsets.all(10),
                   child: ElevatedButton(
                     onPressed: () {
@@ -240,7 +187,7 @@ class _RegisterStateBD extends State<RegisterBD> {
                       ),
                     ),
                   ),
-                ),
+                ),*/
 
               ],
             ),
