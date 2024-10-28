0949fa8df67bf7b435e0179966c7511e15a3b4b1
JuanPabloSaavedraRamirez <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2024-10-27 18:45:47 -0600

    mas continuacion

--- lib/Productos.dart
+++ lib/Productos.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:app_aulagramma/addProducto.dart';
+import 'package:app_aulagramma/datos_producto.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as https;
 
 class Productos extends StatefulWidget {
   const Productos({super.key});
@@ -9,6 +13,33 @@ class Productos extends StatefulWidget {
 }
 
 class _ProductosState extends State<Productos> {
+
+  List<Datos_Producto> datos = [];
+  bool loading = true;
+
+  Future<List<Datos_Producto>> tomar_datos() async {
+    var url = Uri.https('api.aulagrammae.com', 'apps/ver_productos.php');
+    var response = await https.post(url).timeout(Duration(seconds: 90));
+    print(response.body);
+    var datos = jsonDecode(response.body);
+    List<Datos_Producto> registros = [];
+
+    for(datos in datos){
+      registros.add(Datos_Producto.fromJson(datos));
+    }
+
+    return registros;
+  }
+  @override
+  void initState() {
+    super.initState();
+    tomar_datos().then((value){
+      datos.addAll(value);
+      loading = false;
+      print(datos);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/datos_producto.dart
@@ -0,0 +1,15 @@
+class Datos_Producto{
+  String? id;
+  String? nombre;
+  String? descripcion;
+  String? precio;
+
+  Datos_Producto(this.id, this.nombre, this.descripcion, this.precio);
+
+  Datos_Producto.fromJson(Map<String, dynamic> json){
+    id = json['ID'].toString();
+    nombre = json['nombre'];
+    descripcion = json['descripcion'];
+    precio = json['precio'].toString();
+  }
+}
