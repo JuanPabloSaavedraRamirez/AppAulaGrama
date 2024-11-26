a8f4885be252b4245993416645607aca0bdb10a6
JuanPabloSaavedraRamirez <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2024-11-26 12:36:20 -0600

    tercera libreria

--- lib/comprarProductos.dart
+++ lib/comprarProductos.dart
@@ -4,6 +4,7 @@ import 'package:app_aulagramma/datos_producto.dart';
 import 'package:app_aulagramma/menu.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as https;
+import 'package:image_card/image_card.dart';
 
 class ComprarProductos extends StatefulWidget {
   const ComprarProductos({super.key});
@@ -19,7 +20,6 @@ class _ComprarProductosState extends State<ComprarProductos> {
   Future<List<Datos_Producto>> tomar_datos() async {
     var url = Uri.https('api.aulagrammae.com', 'apps/ver_productos.php');
     var response = await https.post(url).timeout(Duration(seconds: 90));
-    print(response.body);
     var datos = jsonDecode(response.body);
     List<Datos_Producto> registros = [];
 
@@ -28,16 +28,6 @@ class _ComprarProductosState extends State<ComprarProductos> {
     }
 
     return registros;
-  }
-
-  Future<void> Foto() async {
-    var url = Uri.https('api.aulagrammae.com', 'apps/ver_foto.php');
-    var response = await https.post(url, body: {
-      //'id': correo,
-      //'foto': pass,
-    });
-    final sensual = jsonDecode(response.body);
-    print('Respuesta completa: ${response.body}');
   }
 
   @override
@@ -71,89 +61,54 @@ class _ComprarProductosState extends State<ComprarProductos> {
         child: ListView.builder(
           itemCount: datos.length,
           itemBuilder: (BuildContext context, int index) {
-            return Card(
-              elevation: 5,
-              margin: EdgeInsets.symmetric(vertical: 8),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
-                    child: Image.network(
+            return Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return confirmarCompra();
+                        },
+                      ),
+                    );
+                  },
+                  child: TransparentImageCard(
+                    width: double.infinity,
+                    height: 250,
+                    borderRadius: 15,
+                    imageProvider: NetworkImage(
                       "https://api.aulagrammae.com/apps/Fotos/${datos[index].foto!}",
-                      height: 200,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
                     ),
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          datos[index].nombre!,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          datos[index].descripcion!,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey.shade600,
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment:
-                          MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "\$${datos[index].precio!}",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue.shade900,
-                              ),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.of(context).pushReplacement(
-                                  MaterialPageRoute(
-                                    builder: (BuildContext context) {
-                                      return confirmarCompra();
-                                    },
-                                  ),
-                                );
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Color(0xFF040C52),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                              child: Text(
-                                "Comprar",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                    title: Text(
+                      datos[index].nombre!,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
+                    description: Text(
+                      datos[index].descripcion!,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white70,
+                      ),
+                    ),
+                    tags: [
+                      Text(
+                        "\$${datos[index].precio!}",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue.shade900,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                SizedBox(height: 10),
+              ],
             );
           },
         ),
